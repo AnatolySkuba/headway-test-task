b935d747f6768335fb311e6ebbb680e96ef52178
Skuba Anatoliy <photomir.in.ua@gmail.com>
2023-06-06 06:55:07 +0300

    feat: added start page

--- src/pages/start/Start.tsx
+++ src/pages/start/Start.tsx
@@ -1,7 +1,28 @@
 import React from "react";
 
+import sprite from "assets/sprite.svg";
+
+import css from "./Start.module.css";
+
 function Start() {
-  return <h1>Start</h1>;
+  return (
+    <div className={css.container}>
+      <svg className={css.picture}>
+        <use href={`${sprite}#hand`} />
+      </svg>
+      <div>
+        <h1 className={css.header}>
+          Who wants to be
+          <br />a millionaire?
+        </h1>
+        <a href="/game">
+          <button type="button" className="btn-primary">
+            Start
+          </button>
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Start;
