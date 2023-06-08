aac32ec831f5b374553b1c920c6ca6f838648d1c
Skuba Anatoliy <photomir.in.ua@gmail.com>
2023-06-08 13:06:17 +0300

    fix: ckeck pre-push

--- src/pages/start/Start.tsx
+++ src/pages/start/Start.tsx
@@ -7,8 +7,6 @@ import { ROUTER_KEYS } from "consts";
 import css from "./Start.module.css";
 
 function Start() {
-  console.log("test2");
-
   return (
     <div className={css.container}>
       <svg className={css.picture}>
